a0ad93ef5a0ba6e711e4eb2956f1ce3183193f4e
Amy Cen <amy.cen@blackrock.com>
2019-04-16 00:07:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,11 @@ def long_planeteer_calls(calls)# code an argument here
   calls.any? { |word| word.length > 4  }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(strings)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    if strings.include?(cheese)
+      gotcha = cheese
+    end
 end
