57e863aa0a00b9e46cf635e4d378bf1965baf14c
Amy Cen <amy.cen@blackrock.com>
2019-04-16 00:18:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,7 @@ end
 
 def find_the_cheese(strings)# code an argument here
   # the array below is here to help
+  gotcha = nil
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
     if strings.include?(cheese)
